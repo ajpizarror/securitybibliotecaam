99803474a989521502fbd01df0f176e5d86fb172
MAGAQ <panamasado41@gmail.com>
2026-06-17 23:50:57 -0400

    Añadiendo config de application y security faltantes, intentare estblecer conexion con api-gateway

---
+++ src/main/java/security/cl/auth/security/config/ApplicationConfig.java
@@ -0,0 +1,35 @@
+package security.cl.auth.security.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import security.cl.auth.security.user.UserDetailsServiceImpl;
+
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config)
+            throws Exception{
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(UserDetailsServiceImpl userDetailsService)
+    {
+        DaoAuthenticationProvider authenticationProvider= new DaoAuthenticationProvider(userDetailsService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+}
