4a13a5f88f4637019d271bffe0c52cd2114855b8
MAGAQ <panamasado41@gmail.com>
2026-06-18 19:20:00 -0400

    Intentando añadir swagger, da error 403

--- src/main/java/security/cl/auth/security/auth/AuthController.java
+++ src/main/java/security/cl/auth/security/auth/AuthController.java
@@ -1,7 +1,14 @@
 package security.cl.auth.security.auth;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.apache.http.auth.AUTH;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,18 +16,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/api/bibliotecaam/auth")
 @RequiredArgsConstructor
+@Tag(name = "Autenticador", description = "Autenticacion y login para los servicios de la Biblioteca AM")
 public class AuthController {
 
     private final AuthService authService;
 
+    @ArraySchema(schema = @Schema(implementation = LoginRequest.class))
+    @Operation(summary = "Iniciar sesion a un trabajador ya registrado de la Biblioteca AM", description = "Esta opcion permitira iniciar sesion a un trabajador que ya tenga una cuenta.")
     @PostMapping(value = "login")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "¡Usuario logeado con exito!"),
+            @ApiResponse(responseCode = "404",description = "El usuario no fue encontrado, pruebe a revisar si ingreso bien sus parametros."),
+            @ApiResponse(responseCode = "400",description = "Puede que hayan faltado algunos parametros, revise que haya introducido todos.")
+    })
     public ResponseEntity<AuthResponse> login(@RequestBody @Valid  LoginRequest request){
         return ResponseEntity.ok(authService.login(request));
     }
 
+    @ArraySchema(schema = @Schema(implementation = LoginRequest.class))
+    @Operation(summary = "Registrar a un trabajador a los servicios de la Biblioteca AM", description = "Esta opcion permitira registrar a un trabajador para que pueda ingresar con su cuenta.")
     @PostMapping(value = "register")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "¡Usuario registrado con exito!"),
+            @ApiResponse(responseCode = "400",description = "El usuario no pudo ser creado, pruebe a revisar si ingreso bien sus parametros.")
+    })
     public ResponseEntity<AuthResponse> register(@RequestBody @Valid RegisterRequest request)
     {
         return ResponseEntity.ok(authService.register(request));
---
+++ src/main/java/security/cl/auth/security/config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package security.cl.auth.security.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI(){
+        return new OpenAPI()
+                .info(new Info().title("API 2026 Autenticador de tokens de seguridad de la Biblioteca AM")
+                        .version("1.0")
+                        .description("Gestion de tokens para manejar la seguridad de los MicroServicios de la Biblioteca AM"));
+    }
+}
