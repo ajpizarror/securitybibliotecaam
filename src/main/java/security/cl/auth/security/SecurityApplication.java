f9197f52912ed794d543f5f1e4eff6ee5e2eb41e
MAGAQ <panamasado41@gmail.com>
2026-06-18 00:14:46 -0400

    Configurandolo para que llegue al api gateway y al eureka, en teoria, MS listo para el Mambo :P

--- src/main/java/security/cl/auth/security/SecurityApplication.java
+++ src/main/java/security/cl/auth/security/SecurityApplication.java
@@ -2,8 +2,12 @@ package security.cl.auth.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableWebSecurity
 public class SecurityApplication {
 
 	public static void main(String[] args) {
--- src/main/java/security/cl/auth/security/config/ApplicationConfig.java
+++ src/main/java/security/cl/auth/security/config/ApplicationConfig.java
@@ -2,6 +2,7 @@ package security.cl.auth.security.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -10,6 +11,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import security.cl.auth.security.user.UserDetailsServiceImpl;
 
+@Configuration
 @RequiredArgsConstructor
 public class ApplicationConfig {
 
