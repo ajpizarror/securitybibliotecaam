77511bc28d760488ca58673e9410f70edc349982
MAGAQ <panamasado41@gmail.com>
2026-06-23 22:29:22 -0400

    Register funciona, falta arreglar login

--- src/main/java/security/cl/auth/security/config/SwaggerConfig.java
+++ src/main/java/security/cl/auth/security/config/SwaggerConfig.java
@@ -5,13 +5,22 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import java.util.List;
 @Configuration
 public class SwaggerConfig {
+
+    @Value("${springdoc.servers[0].url:http://localhost:8098}")
+    private String serverUrl;
+
+    @Value("${springdoc.servers[0].description:Via del Api Gateway}")
+    private String serverDescription;
+
     @Bean
-    public OpenAPI customOpenAPI(){
+    public OpenAPI customOpenAPI() {
         SecurityScheme securityScheme = new SecurityScheme()
                 .name("bearerAuth")
                 .type(SecurityScheme.Type.HTTP)
@@ -20,10 +29,17 @@ public class SwaggerConfig {
         SecurityRequirement securityRequirement = new SecurityRequirement()
                 .addList("bearerAuth");
         return new OpenAPI()
-                .info(new Info().title("API 2026 Autenticador de tokens de seguridad de la Biblioteca AM")
+                .info(new Info()
+                        .title("API 2026 Autenticador de tokens de seguridad de la Biblioteca AM")
                         .version("1.0")
                         .description("Gestion de tokens para manejar la seguridad de los MicroServicios de la Biblioteca AM"))
-                .components(new Components().addSecuritySchemes("bearerAuth",securityScheme))
+                .servers(List.of(
+                        new Server()
+                                .url(serverUrl)
+                                .description(serverDescription)
+                ))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", securityScheme))
                 .addSecurityItem(securityRequirement);
     }
 }
